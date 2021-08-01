1d5fc4e75890775c958bc7cb5896a9e92ad1a0b5
ksdfg <ksdfg123@gmail.com>
2021-08-01 14:57:16 +0530

    feat(config): fetch config and pass to function instead of storing in package level variable
    Using an unexported package level variable just felt kinda wrong, and this way we can avoid the multiple config.GetConfig calls
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- config/config.go
+++ config/config.go
@@ -11,40 +11,38 @@ type Config struct {
 	DBUri string `mapstructure:"DB_URI"`
 }
 
-var cfg *Config = nil
-
 /*
 GetConfig will set all config variables in cfg if not already set, and cfg
 */
-func GetConfig() *Config {
-	if cfg == nil {
-		// Setup viper
-		viper.SetConfigName(".env")
-		viper.SetConfigType("env")
-		viper.AddConfigPath(".")
+func GetConfig() Config {
+	var cfg Config
 
-		// Set default values for config vars
-		viper.SetDefault("DB_URI", "")
+	// Setup viper
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
 
-		// Automatically override values in config file with those in environment
-		viper.AutomaticEnv()
+	// Set default values for config vars
+	viper.SetDefault("DB_URI", "")
 
-		// Read config file
-		err := viper.ReadInConfig()
-		if err != nil {
-			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-				// Config file not found; ignore error if desired
-			} else {
-				// Config file was found but another error was produced
-				log.Fatal(err)
-			}
-		}
+	// Automatically override values in config file with those in environment
+	viper.AutomaticEnv()
 
-		// Set config object
-		err = viper.Unmarshal(&cfg)
-		if err != nil {
+	// Read config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+		} else {
+			// Config file was found but another error was produced
 			log.Fatal(err)
 		}
+	}
+
+	// Set config object
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	return cfg
