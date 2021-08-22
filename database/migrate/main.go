a96f0dbdf4c921305be01e198dce85eb4e303b2e
ksdfg <ksdfg123@gmail.com>
2021-08-22 16:38:34 +0530

    fix: connect to database before migrating models
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- database/migrate/main.go
+++ database/migrate/main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"management-backend/config"
 	"management-backend/database"
 	"management-backend/models"
 )
+
+func init() {
+	// Configure logging
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.SetOutput(os.Stderr)
+
+	// Fetch config vars
+	config.Init()
+
+	// Connected with database
+	database.Connect()
+}
 
 func main() {
 	db := database.Get()
 
 	// Add UUID extension to postgres
-	result := db.Raw("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";").Scan(nil)
+	result := db.Raw("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
