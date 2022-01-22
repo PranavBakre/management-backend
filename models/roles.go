a3ee7ddb48eeaaca8aa08193bfe3c85840f90b8d
ksdfg <ksdfg123@gmail.com>
2022-01-22 18:20:49 +0530

    feat(models): cascade on delete for many to many relationships
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- models/roles.go
+++ models/roles.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type Role struct {
 	ID     uuid.UUID `gorm:"primaryKey;default:uuid_generate_v4()"`
 	Role   string    `gorm:"not null;index;unique;"`
-	Rights []Right   `gorm:"default:null;many2many:role_right;"`
+	Rights []Right   `gorm:"default:null;many2many:role_right;constraint:OnDelete:CASCADE;"`
 }
 
 func (Role) TableName() string {
--- models/user.go
+++ models/user.go
@@ -14,7 +14,7 @@ type User struct {
 	Email     string         `gorm:"default:null;not null" json:"email" form:"email"`
 	GoogleID  *string        `gorm:"index;unique" json:"google_id" form:"google_id"`
 	Picture   *string        `gorm:"default:null" json:"picture" form:"picture"`
-	Roles     []Role         `gorm:"default:null;many2many:user_role;"`
+	Roles     []Role         `gorm:"default:null;many2many:user_role;constraint:OnDelete:CASCADE;"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at,omitempty"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
